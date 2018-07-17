9f7d59deb73f459c09a6684233a219f2810a1b0b
Oleksii Popov <oleksiipopov@admins-mbp.synapse.com>
2018-07-17 11:03:47 +0300

    functions createListItem and addToDoItem was added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,42 @@
 function createTodoItem (title) {
-    const 
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox';
+    
+    const label = document.createElement('label');
+    label.innerText = title;
+    label.className = 'title';
+
+    const editInput = document.createElement('input');
+    editInput.type = 'text';
+    editInput.className = 'textfield';
+
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Change';
+    editButton.className = 'edit';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.className = 'delete';
+
+    const listItem = document.createElement('li');
+    listItem.className = 'todo-item';
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+    console.log(listItem);
+    return listItem;
+
 }
 function addTodoItem (event) {
     event.preventDefault();
     
     if(addInput.value === '') return alert("Nesessary add task name");
 
-    const listItem = createTodoItem(addInpu.value)
+    const listItem = createTodoItem(addInput.value)
 }
 
 const todoForm = document.querySelector('#todo-form');
