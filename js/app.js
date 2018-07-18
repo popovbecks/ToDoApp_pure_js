7ecdc8805550ed61559d973591d594f13aab4399
Oleksii Popov <oleksiipopov@admins-mbp.synapse.com>
2018-07-18 14:27:29 +0300

    add default message window and change some styles

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 const main = ((document) => {
-    function createElement (tag, props, ...children) {
+    function createElement(tag, props, ...children) {
         const element = document.createElement(tag);
-        
-        for(let prop in props) {
-            if(!props.hasOwnProperty(prop)) continue;
+        //we get only own properties
+        for (let prop in props) {
+            if (!props.hasOwnProperty(prop)) continue;
             element[prop] = props[prop]; //element.type = props['type'];
         }
-    
+
         if (children.length > 0) {
             children.forEach(child => {
-                if(typeof child === 'string') {
+                if (typeof child === 'string') {
                     child = document.createTextNode(child);
                 }
                 element.appendChild(child);
@@ -17,83 +17,112 @@ const main = ((document) => {
         }
         return element;
     }
-    
-    function createTodoItem (title) {
-        const checkbox = createElement('input', { type: 'checkbox', className:'checkbox' });
-        
-        const label = createElement('label', { className: 'title' }, title);
-    
-        const editInput = createElement('input', { type: 'text', className: 'textfield' });
-    
-        const editButton = createElement('button', { className: 'edit' }, 'Change');
-    
-        const deleteButton = createElement('button', { className: 'delete' }, 'Delete');
-    
-        const listItem = createElement('li', { className: 'todo-item' }, checkbox, label, editInput, editButton, deleteButton);
-    
+
+    function createTodoItem(title) {
+        const checkbox = createElement('input', {
+            type: 'checkbox',
+            className: 'checkbox'
+        });
+
+        const label = createElement('label', {
+            className: 'title'
+        }, title);
+
+        const editInput = createElement('input', {
+            type: 'text',
+            className: 'textfield'
+        });
+
+        const editButton = createElement('button', {
+            className: 'edit'
+        }, 'Change');
+
+        const deleteButton = createElement('button', {
+            className: 'delete'
+        }, 'Delete');
+
+        const listItem = createElement('li', {
+            className: 'todo-item'
+        }, checkbox, label, editInput, editButton, deleteButton);
+
         bindEvents(listItem);
         return listItem;
-    
+
     }
+
+    //use events delegation
     function bindEvents(todoItem) {
-        const checkbox = todoItem.querySelector('.checkbox');
-        const editButton = todoItem.querySelector('button.edit');
-        const deleteButton = todoItem.querySelector('button.delete');
-    
-        checkbox.addEventListener('change', toggleTodoItem);
-        editButton.addEventListener('click', editTodoItem);
-        deleteButton.addEventListener('click', deleteTodoItem);
-    
+        todoItem.addEventListener('change', toggleTodoItem);
+        todoItem.addEventListener('click', editTodoItem);
+        todoItem.addEventListener('click', deleteTodoItem);
     }
-    function addTodoItem (event) {
+
+    function addTodoItem(event) {
+        //canceled send operation by default
         event.preventDefault();
+
+        if (addInput.value === '') return alert("Nesessary add task name");
         
-        if(addInput.value === '') return alert("Nesessary add task name");
-    
         const todoItem = createTodoItem(addInput.value);
         todoList.appendChild(todoItem);
         addInput.value = '';
+        if (todoList.children.length) {
+            message.classList.add('hide');
+        }
     }
-    
-    //use restructuring, instead event we use {target}
-    function toggleTodoItem ({ target }) {
+
+    //use restructuring, instead event we use {target} from event object
+    function toggleTodoItem({
+        target
+    }) {
+        if (target.className != 'checkbox') return;
         const listItem = target.parentNode;
         listItem.classList.toggle('completed');
     }
-    
-    //use "this" like access to event element
-    function editTodoItem () {
-        const listItem = this.parentNode;
+
+
+    function editTodoItem({
+        target
+    }) {
+        if (target.className != 'edit') return;
+        const listItem = target.parentNode;
         const title = listItem.querySelector('.title');
         const editInput = listItem.querySelector('.textfield');
         const isEditing = listItem.classList.contains('editing');
-    
+
         if (isEditing) {
             title.innerText = editInput.value;
-            this.innerText = 'Change';
+            target.innerText = 'Change';
         } else {
             editInput.value = title.innerText;
-            this.innerText = 'Save';
-    
+            target.innerText = 'Save';
+
         }
         listItem.classList.toggle('editing');
     }
-    
-    
-    function deleteTodoItem () {
-        const listItem = this.parentNode;
+
+
+    function deleteTodoItem({ target }) {
+        if (target.className != 'delete') return;
+        
+        const listItem = target.parentNode;
         todoList.removeChild(listItem);
+        if (!todoList.children.length) {
+            message.classList.remove('hide');
+        }
     }
+
     const todoForm = document.querySelector('#todo-form');
     const addInput = document.querySelector('#add-input');
     const todoList = document.querySelector('#todo-list');
     const todoItems = document.querySelectorAll('.todo-item');
-    
-    function main () {
+    const message = document.querySelector('.todo-message');
+
+    function main() {
         todoForm.addEventListener('submit', addTodoItem);
-        todoItems.forEach((item)=>bindEvents(item))
+        todoItems.forEach((item) => bindEvents(item))
     }
-    
-   return main;
+
+    return main;
 })(document);
 main();
